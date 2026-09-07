ec8073baad71feee1674af03cbfbf829fb0cbd1a
malvaaskerup <malvaaskerup@gmail.com>
2026-09-07 17:57:15 +0200

    commit

--- api/confirm.js
+++ api/confirm.js
@@ -83,7 +83,7 @@ async function sendApprovedEmail(booking) {
     Faktura eller Företagsswish, innan ankomst — om inget annat
     överenskommits. Moms med 6% ingår för privatpersoner, men läggs på
     över totalsumman för företag.<br><br>
-    <strong>Swish till Medicinsk PT Askerup:”</strong> <a href="tel:+123 171 08 39">123 171 08 39</a><br><br>
+    <strong>Swish till Medicinsk PT Askerup:</strong> 123 171 08 39<br><br>
     För betalningar från utlandet går det bra med PayPal (sök på Fredric
     Askerup, eller användarnamnet fredricaskerup).</p>
     <p>Så snart betalningen är registrerad hos oss skickar vi en sista
